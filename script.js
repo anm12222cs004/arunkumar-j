ca5a94f6a5e4503ac9b998b7cb683cfb3befe432
anm12222cs004 <144416758+anm12222cs004@users.noreply.github.com>
2025-03-24 15:18:44 +0530

    Update and rename arun.js to script.js

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ function displayWeather(data) {
     weatherResult.innerHTML = `
 <h2>${data.name}</h2>
 <img src="http://openweathermap.org/img/wn/${icon}.png" alt="${description}">
-<p>Temperature: ${temperature} °C</p>
+<p>Temperature: ${temperature} Â°C</p>
 <p>Humidity: ${humidity} %</p>
 <p>Description: ${description.charAt(0).toUpperCase() + description.slice(1)}</p>
     `;
